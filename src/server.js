de336abd8928b815f7b104bab0ac46a0eb4f0699
sina <cna.so@outlook.com>
2024-04-03 20:05:21 +0330

    002 -- set socketIO server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
-import express  from "express"
+import express from "express"
+import logger from "morgan"
 import {join} from "path"
+import {Server as SocketIoServer} from "socket.io"
+import http from "http"
 const PORT = 4000
 
 const app = express()
 
-app.set("view engine" , "pug")
-app.set("views" , join(__dirname , "views"))
-app.use(express.static(join(__dirname , "static")))
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 
 
 
-app.get("/",(req , res) => res.render("home"))
-const handleServer = () => {
-    console.log(`Run server on port : ${PORT}`)
-}
+app.get("/", (req, res) => res.render("home"))
+const server = http.createServer(app)
+const io = new SocketIoServer(server, {})
+io.on("connection" , (socket) => console.log(socket.id))
 
-app.listen(PORT , handleServer)
+server.listen(3000, () => {
+    console.log(` server start on port ${PORT}`)
+})
+
+
